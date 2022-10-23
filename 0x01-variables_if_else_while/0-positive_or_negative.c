047422ab7c50ce4b209bb0768ec7ae528b3a381f
Ali-Wario <halimawario8@gmail.com>
2022-10-23 02:28:18 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n==0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return(0);
+}
+

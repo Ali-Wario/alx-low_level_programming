c8147190a4e7af84e43b7fee7616938ce125eea3
Ali-Wario <114773924+Ali-Wario@users.noreply.github.com>
2022-10-31 11:04:44 +0300

    My first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: 0
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
 	int n;
-	srand(time(0));
-	n= rand()-RAND_MAX /2 ;
-
+srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -24,6 +23,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return(0);
+	return (0);
 }
-
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-My first readme
+The readme for the alx 0x01-variables_if_else_while task that teachesthe various  loops in C and their functions
